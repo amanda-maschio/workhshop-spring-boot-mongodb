33af82404c1d94735aad9b04d562e4afd215f799
Amanda Maschio <maschioamanda@hotmail.com>
2021-06-16 15:07:45 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/cursoudemy/workshopmongo/resources/UserResource.java
+++ src/main/java/com/cursoudemy/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.cursoudemy.workshopmongo.resources;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursoudemy.workshopmongo.domain.User;
+import com.cursoudemy.workshopmongo.dto.UserDTO;
 import com.cursoudemy.workshopmongo.services.UserService;
 
 //Anotação indicando um Recurso REST
@@ -22,10 +24,26 @@ public class UserResource {
 	
 	//ResponseEntity é um tipo específico do Spring para retornar respostas de requisições web
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 
 		List<User> list = service.findAll();
+		List<UserDTO> listDTO = new ArrayList<>();
 
-		return ResponseEntity.ok().body(list);
+		for (int i = 0; i < list.size(); i++) {
+
+			User user = list.get(i);
+			UserDTO userDTO = new UserDTO();
+
+			userDTO.setId(user.getId());
+			userDTO.setName(user.getName());
+			userDTO.setEmail(user.getEmail());
+
+			listDTO.add(userDTO);
+		}
+		
+		// Expressão lambda alternativa para a conversão da list<User> para list<UserDTO>
+		// list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
