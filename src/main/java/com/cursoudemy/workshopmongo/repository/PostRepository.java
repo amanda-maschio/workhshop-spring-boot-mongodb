3bc878fc9c7307d21a26a828d8813861bfdcb759
Amanda Maschio <maschioamanda@hotmail.com>
2021-06-17 00:31:58 -0300

    Consulta simples com @Query

--- src/main/java/com/cursoudemy/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/cursoudemy/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.cursoudemy.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cursoudemy.workshopmongo.domain.Post;
@@ -14,6 +15,10 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	//MongoRepository já tem uma implementação padrão para essa interface
 	
 	//QUERY METHODS
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
